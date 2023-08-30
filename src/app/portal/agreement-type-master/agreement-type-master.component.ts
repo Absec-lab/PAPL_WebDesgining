e8c538851950d25c8e856799d43f1dd67f1f523d
27sep <somyaranjan1212@gmail.com>
2023-08-30 20:22:52 +0530

    all

--- src/app/portal/agreement-type-master/agreement-type-master.component.ts
+++ src/app/portal/agreement-type-master/agreement-type-master.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AgreementTypeMasterComponent {
   tableData = [
-    { SL_NO: 1, Unit_No: 'ROOM_1', Occupied: '5',Available:'5', Action:''},
+    { SL_NO: 1, Agreement_Type: 'Agreement_1', Description: 'with_ele',Start_Date:'02/08/2023',End_Date:'02/09/2023',Created_Date:'11/08/2023', Action:''},
+    { SL_NO: 2, Agreement_Type: 'Agreement_2', Description: 'Jharsuguda',Start_Date:'02/08/2023',End_Date:'02/09/2023',Created_Date:'11/08/2023', Action:''},
+    { SL_NO: 3, Agreement_Type: 'Agreement_3', Description: 'Rourkela',Start_Date:'02/08/2023',End_Date:'02/09/2023',Created_Date:'11/08/2023', Action:''},
     
     // Add more data items as needed
   ];

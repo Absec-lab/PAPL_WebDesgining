47865a42c9210272e4090ef4115805d2c767372b
bibhuti_bapi <bibhuti.worksafe@gmail.com>
2023-10-09 12:07:49 +0530

    Code updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: false,
   url: '',
   
-  // New Local test 7001 Mrutunjay Pani
   serviceURL: 'http://206.189.142.35:9090/',
   siteURL: 'http://localhost:4200/',
 

65bea674425100875f2823ac6a6f8a9dd871d12f
sandeepsahoo215 <sandeepsahoo215@gmail.com>
2023-11-19 00:11:42 +0530

    house owner registration

--- src/app/portal/house-owner-registration/house-owner-registration.component.ts
+++ src/app/portal/house-owner-registration/house-owner-registration.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { PortalServiceService } from './../serviceapi/portal-service.service';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
-import { FormBuilder, FormGroup,FormControl , Validators } from '@angular/forms';
+import { FormBuilder, FormGroup,FormControl , Validators, FormArray } from '@angular/forms';
 import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 import { HttpClient } from "@angular/common/http";
@@ -24,6 +24,8 @@ export class HouseOwnerRegistrationComponent {
   houseRegistrationForm!: FormGroup<any>;
   updatebtn:boolean = false;
   descripinput:boolean = false;
+  legalheirForm!: FormGroup;
+  noOfLegalParties:any;
   constructor(private ngxLoader: NgxUiLoaderService, private formBuilder: FormBuilder, private route: Router, public portalServ: PortalServiceService, private httpClient: HttpClient, public vldChkLst: ValidatorchklistService) { }
   ngOnInit(): void {
     this.getAllOwner();
@@ -52,10 +54,55 @@ export class HouseOwnerRegistrationComponent {
       upiId: [''],
       linkMobile: [''],
       qrCode: [''],
+      uploadlegalheir:['']
      
     });
+
+    this.legalheirForm = this.formBuilder.group({
+      legal:this.formBuilder.array([])
+    })
   }
- 
+  
+  get legalheirarray() : any {
+    return this.legalheirForm.get('legal') as FormArray;
+  }
+
+  addlegaleirform() {
+    const legalform = this.formBuilder.group({
+      prevOwonerId:[this.houseRegistrationForm.value.ownerId],
+      ownerName:[''],
+      phoneNo:[''],
+      emailId:[''],
+      idProofDoc:[''],
+      idProof:[''],
+      address1:[''],
+      address2:[''],
+      stateId:[''],
+      district:[''],
+      pinCode:[''],
+      paymtMode:[''],
+      isActive:[''],
+      accountHolderName:[''],
+      bankAccountNo:[''],
+      ifscCode:[''],
+      panNo:[''],
+      panCardAddress:[''],
+      upiId:[''],
+      upiPhoneNo:[''],
+      uploadQuarCodeAdds:[''],
+      
+    })
+    console.log(typeof this.noOfLegalParties);
+    console.log( this.noOfLegalParties);
+       this.legalheirarray.insert(0, legalform);
+    // if( this.legalheirarray.length > this.noOfLegalParties ) {
+
+    // }
+  }
+
+  removelegalform(index:number) {
+    this.legalheirarray.removeAt(index)
+  }
   bankStatusChk: any = 0;
   getAllStateList() {
     this.portalServ.get('PAPL/getAllState')
@@ -83,27 +130,77 @@ export class HouseOwnerRegistrationComponent {
    }); 
   }
 
-  onImageChange(event: any): void {
-		const inputElement = event.target as HTMLInputElement;
+  onImageChange(event: any, formControlName: string, formArrName?: string, index?: number): void {
+    console.log("formControlName",formControlName);
+    
+    const inputElement = event.target as HTMLInputElement;
+  
     if (inputElement.files && inputElement.files.length > 0) {
-		const file = inputElement.files[0];
-	  
-		if (file) {
-		  // You can optionally display a preview of the selected image
-		  // For example, you can use FileReader to read the file and set it as a preview.
-	  
-		  const reader = new FileReader();
-		  reader.onload = (e) => {
-			// Set the image form control's value to the file
-			this.houseRegistrationForm.value.gIdproof.setValue(file);
-		  };
-		  reader.readAsDataURL(file);
-		}
+      const file = inputElement.files[0];
+     
+      if (file) {
+        const reader = new FileReader();
+  
+        reader.onload = (e) => {
+          const imageDataUrl = e.target?.result as string;
+          console.log(imageDataUrl);
+          
+          if (formArrName && index !== undefined) {
+            // Update image value in a FormArray at a specific index
+            const formArray = this.legalheirForm.get(formArrName) as FormArray;
+  
+            if (formArray && index >= 0 && index < formArray.length) {
+              const formGroup = formArray.at(index) as FormGroup;
+              formGroup.get(formControlName)?.setValue(imageDataUrl);
+            }
+          } else {
+            // Update image value in a regular FormControl
+            const formControl = this.houseRegistrationForm.get(formControlName);
+  
+            if (formControl) {
+              formControl.setValue(imageDataUrl);
+            }
+          }
+        };
+  
+        reader.readAsDataURL(file);
+      }
     }
-	  }
+    console.log(this.houseRegistrationForm);
+    
+  }
+
+  validateData() {
+    const formControls = [
+      { control: this.houseRegistrationForm.get('ownerName'), name: "Owner Name" },
+      { control: this.houseRegistrationForm.get('phone'), name: "Phone" },
+      { control: this.houseRegistrationForm.get('email'), name: "Email" },
+      { control: this.houseRegistrationForm.get('state'), name: "State" },
+      { control: this.houseRegistrationForm.get('paymode'), name: "Paymode" },
+     
+    ];
+     
+    let vSts = true;
+  
+    for (const formControl of formControls) {
+      if(formControl.control?.valid) {
+        vSts = true;
+      } else {
+        Swal.fire({
+          // icon: 'error',
+          text: `Please select ${formControl.name}`
+        });
+        vSts = false
+        break;
+      }
+      
+    }
+    return vSts;
+  }
 
   ownerRegistration() {
-    if(this.houseRegistrationForm.valid) {
+    let valid = this.validateData();
+    if(valid) {
       let data:any =[
         {
         
@@ -136,23 +233,27 @@ export class HouseOwnerRegistrationComponent {
           "noofLegalParties": 0,
         }
       ]
-
+      this.ngxLoader.start();
       this.portalServ.post("PAPL/addOwners",data)
       .subscribe((res)=>{
+        this.ngxLoader.stop();
       console.log(res)
-      alert("House Owner Registation succcesfull")
       this.getAllOwner();
       this.houseRegistrationForm.reset()
+      Swal.fire({
+        icon: 'success',
+        text: 'Record Saved Successfully'
+      });
+      
     })
-    } else {
-      alert("Please Enter Required field")
-    }
+    } 
   }
   
   editOwner(item:any){
     console.log(item)
     this.updatebtn = true;
     this.descripinput = true;
+    this.paymentmode(item.paymtMode == 'string' ? 2 :item.paymtMode ,'owner')
     this.houseRegistrationForm.patchValue({
       ownerId:item.ownerId,
       ownerName: item.ownerName,
@@ -165,7 +266,7 @@ export class HouseOwnerRegistrationComponent {
       state: item.state.stateId,
       dist: item.district,
       pin: item.pinCode,
-      paymode: item.paymtMode,
+      paymode:item.paymtMode == 'string' ? 2 :item.paymtMode,
       status: '',
       accHolName: item.accountHolderName,
       accounNum: item.bankAccountNo,
@@ -180,32 +281,35 @@ export class HouseOwnerRegistrationComponent {
   }
   
   updateowner() {
+
     let data = {
-      "accountHolderName": this.houseRegistrationForm.value.accHolName,
-      "address1": this.houseRegistrationForm.value.add1,
-      "address2": this.houseRegistrationForm.value.add2,
-      "bankAccountNo":  this.houseRegistrationForm.value.accounNum,
-      "description": this.houseRegistrationForm.value.desc,
-      "district": this.houseRegistrationForm.value.dist,
-      "emailId": this.houseRegistrationForm.value.email,
-      "idProof": this.houseRegistrationForm.value.gIdproof,
-      "idProofAddress":this.houseRegistrationForm.value.idProof,
-      "ifscCode":this.houseRegistrationForm.value.ifsc,
-      "legalIdProof": "",
-      "legalIdProofAddr": "",
-      "noofLegalParties": 0,
-      "ownerId": this.houseRegistrationForm.value.ownerId,
-      "ownerName":this.houseRegistrationForm.value.ownerName,
-      "panCardAddress": this.houseRegistrationForm.value.panPic,
-      "panNo":  this.houseRegistrationForm.value.pan,
-      "paymtMode": this.houseRegistrationForm.value.paymode,
-      "phoneNo":this.houseRegistrationForm.value.phone,
-      "pinCode":this.houseRegistrationForm.value.pin,
-      "prevOwonerId": '',
-      "state": {
-        "stateId": this.houseRegistrationForm.value.state,
+      "legalHeirRequestDto":this.legalheirarray.value ,
+      "ownerRegistrationRequestDto": {
+        "ownerId":  this.houseRegistrationForm.value.ownerId,
+        "ownerName": this.houseRegistrationForm.value.ownerName,
+        "phoneNo": this.houseRegistrationForm.value.phone,
+        "emailId": this.houseRegistrationForm.value.email,
+        "paymtMode": this.houseRegistrationForm.value.paymode,
+        "address1":  this.houseRegistrationForm.value.add1,
+        "address2":  this.houseRegistrationForm.value.add2,
+        "stateId":  this.houseRegistrationForm.value.state,
+        "district": this.houseRegistrationForm.value.dist,
+        "pinCode":this.houseRegistrationForm.value.pin,
+        "idProof":  this.houseRegistrationForm.value.gIdproof,
+        "idProofDoc": this.houseRegistrationForm.value.idProof,
+        "isActive": false,
+        "accountHolderName":this.houseRegistrationForm.value.accHolName,
+        "bankAccountNo": this.houseRegistrationForm.value.accounNum,
+        "ifscCode": this.houseRegistrationForm.value.ifsc,
+        "panNo":  this.houseRegistrationForm.value.pan,
+        "panCardAddress":this.houseRegistrationForm.value.panPic,
+        "upiId":this.houseRegistrationForm.value.upiId,
+        "upiPhoneNo": this.houseRegistrationForm.value.linkMobile,
+        "uploadQuarCodeDoc": this.houseRegistrationForm.value.qrCode,
+        "legalIdProof": "testing",
+        "legalIdProofDoc": "testing",
+        "noofLegalParties": this.noOfLegalParties
       }
-    
     }
     
     this.portalServ.put("PAPL/updateOwner",data)
@@ -225,21 +329,33 @@ export class HouseOwnerRegistrationComponent {
       this.legalheir = true;
     } else {
       this.legalheir = false;
+      if(this.legalbtnh) {
+        this.legalbtnh = false
+      }
     }
   }
 
   togglelegal() {
     this.legalbtnh = !this.legalbtnh
     this.legalheir = false;
-    this.descripinput= false
+    this.addlegaleirform()
+   // this.descripinput= false
   }
   legalheading= false;
   legalbank = false;
   legalupi = false;
-  paymentmode(event:any,userType:string) {
-    console.log(event.target.value, userType)
-    let value = event.target.value;
+  paymentmode(event:any,userType:string, index?:any) {
+    console.log("image");
+    let value:any;
+    console.log(event, userType, typeof(event))
+    if(typeof(event)=='number') {
+      value = event
+    } else {
 
+      value = event.target.value;
+    }
+    
+   
     if(userType=='owner') {
       if(value == '2') {
         this.heading = false 
@@ -255,25 +371,41 @@ export class HouseOwnerRegistrationComponent {
         this.upi = true;
       }
     } else if(userType=='legal' ) {
-      if(value == '2') {
-        this.legalheading = false 
-      } else {
-      this.legalheading = true 
-      }
+      // if(value == '2') {
+      //   this.legalheading = false 
+      // } else {
+      // this.legalheading = true 
+      // }
 
-      if(value=='1') {
-        this.legalbank = true
-        this.legalupi = false
-      } else {
-        this.legalbank = false;
-        this.legalupi = true;
-      }
+      // if(value=='1') {
+        
+      //   this.legalbank = true
+      //   this.legalupi = false
+      // } else {
+      //   this.legalbank = false;
+      //   this.legalupi = true;
+      // }
     }
+
+    
+    
   }
 		
     
 
-  
+  showhidebankdetails(indes:any) {
+   // this.legalheirarray.constrols
+   console.log([3,4,5,6].includes(this.legalheirarray.value[indes].paymtMode));
+   console.log(typeof this.legalheirarray.value[indes].paymtMode);
+    if(this.legalheirarray.value[indes].paymtMode == 1) {
+      return "Bank"
+    }  else if([3,4,5,6].includes(+this.legalheirarray.value[indes].paymtMode) ) {
+     
+      
+      return "upi"
+    }
+      return 'false'
+  }
 
   
 
